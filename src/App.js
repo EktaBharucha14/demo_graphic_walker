56f0a7afc3d9dd41af367467b3df5d083f9ae31c
Ekta Bharucha <ekta.bharucha@trootech.com>
2024-02-23 15:30:16 +0530

    Initial code for package

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import GraphicWalkerComponent from "./GraphicWalker";
+
+function App() {
+  return (
+    <div className="App">
+      <GraphicWalkerComponent />
+    </div>
+  );
+}
+
+export default App;
